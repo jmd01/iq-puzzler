95cdf85840fd615d5f1eeb79a0cc2ee2d75766cf
John Davison <john.davison@deskpro.com>
2023-10-28 10:44:35 +0100

    Added version number

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ export const metadata = {
   description: "Test your IQ yo",
 };
 
+const VERSION = "1.0.0";
+
 export default function RootLayout({
   children,
 }: {
@@ -18,6 +20,18 @@ export default function RootLayout({
         style={{ height: "100vh", overflow: "hidden" }}
       >
         {children}
+        <div
+          style={{
+            position: "absolute",
+            bottom: 0,
+            right: 0,
+            opacity: 0.3,
+            color: "#FFF",
+            fontSize: 12,
+          }}
+        >
+          {VERSION}
+        </div>
       </body>
     </html>
   );
